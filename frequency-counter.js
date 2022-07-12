a9686139ee1f88a5899950f0122f30febbfec091
Barbara Szewczyk <comamysza@gmail.com>
2022-07-12 22:24:53 +0200

    adds solution without, but square can be one for more arg from first arr

--- frequency-counter.js
+++ frequency-counter.js
@@ -4,20 +4,30 @@ function same(initialArray, squaredValuesGoal) {
 	// check if arrays contain only numbers
 	const isArr1Num = initialArray.every((val) => !isNaN(val));
 	const isArr2Num = squaredValuesGoal.every((val) => !isNaN(val));
-	console.log(isArr1Num, isArr2Num);
+	//console.log(isArr1Num, isArr2Num);
 
 	if (!isArr1Num || !isArr2Num) {
 		console.log('Arrays should contain only numbers');
 		return;
 	}
-	// sort both arrays
-	const sortInitialArray = initialArray.sort((a, b) => a - b);
-	const sortSquaredValuesGoalArray = squaredValuesGoal.sort((a, b) => a - b);
-	console.log(sortInitialArray, sortSquaredValuesGoalArray);
+	// // sort both arrays
+	// const sortInitialArray = initialArray.sort((a, b) => a - b);
+	// const sortSquaredValuesGoalArray = squaredValuesGoal.sort((a, b) => a - b);
+	// console.log(sortInitialArray, sortSquaredValuesGoalArray);
 
-	// create variable which hold squares of initial array
-	// compare if squaredValuesGoal contains initialArraySquared
+	// // create variable which hold squares of initial array
+	// const initalArraySquared = initialArray.map((num) => num * num);
+	// console.log(initalArraySquared);
+
+	// compare if squaredValuesGoal contains initialArraySquared{
+	const squareAvailable = initialArray.every((num) =>
+		squaredValuesGoal.includes(num * num)
+	);
 	// return true if every initialArray has its square in the squaredValueGoal
+	console.log(squareAvailable);
+	return squareAvailable;
 }
 
 same([4, 1, 2, 3], [16, 1, 4, 9]);
+same([4, 1, 2, 3, 10], [16, 1, 4, 9]);
+same([4, 1, 2, 3, 3], [16, 1, 4, 9]);
