b97d732ffed121e650d52cb9445a7dd2314aceb2
Barbara Szewczyk <comamysza@gmail.com>
2022-07-12 21:45:24 +0200

    adds approach pseudo-code

--- frequency-counter.js
+++ frequency-counter.js
@@ -1,1 +1,11 @@
 // Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding calue squared in the second array. The frequency of values must be the same
+
+function same(initialArray, squaredValuesGoal) {
+	// check if arrays contain only numbers
+	// sort both arrays
+	// create variable which hold squares of initial array
+	// compare if squaredValuesGoal contains initialArraySquared
+	// return true if every initialArray has its square in the squaredValueGoal
+}
+
+same([1, 2, 3], [1, 4, 9]);
