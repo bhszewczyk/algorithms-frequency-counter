4644d918cd42f33051253df502d76af55a823fcf
Barbara Szewczyk <comamysza@gmail.com>
2022-07-12 22:03:43 +0200

    adds check if provided arrays are nume

--- frequency-counter.js
+++ frequency-counter.js
@@ -2,10 +2,18 @@
 
 function same(initialArray, squaredValuesGoal) {
 	// check if arrays contain only numbers
+	const isArr1Num = initialArray.every((val) => !isNaN(val));
+	const isArr2Num = squaredValuesGoal.every((val) => !isNaN(val));
+	console.log(isArr1Num, isArr2Num);
+
+	if (!isArr1Num || !isArr2Num) {
+		console.log('Arrays should contain only numbers');
+		return;
+	}
 	// sort both arrays
 	// create variable which hold squares of initial array
 	// compare if squaredValuesGoal contains initialArraySquared
 	// return true if every initialArray has its square in the squaredValueGoal
 }
 
-same([1, 2, 3], [1, 4, 9]);
+same([4, 1, 2, 3], [16, 1, 4, 9]);
