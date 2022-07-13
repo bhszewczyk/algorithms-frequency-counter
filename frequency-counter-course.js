caac645f5ea9777dd01b96d5451578a02fd59a7c
Barbara Szewczyk <comamysza@gmail.com>
2022-07-13 20:44:44 +0200

    adds course naive solution

---
+++ frequency-counter-course.js
@@ -0,0 +1,16 @@
+function same(arr1, arr2) {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	for (let i = 0; i < arr1.length; i++) {
+		let correctIndex = arr2.indexOf(arr1[i] ** 2);
+		if (correctIndex === -1) {
+			return false;
+		}
+		arr2.splice(correctIndex, 1);
+	}
+	return true;
+}
+
+console.log(same([2, 1, 2, 3], [9, 4, 1, 4, 12]));
+console.log(same([2, 1, 2, 3], [9, 4, 1, 4]));
