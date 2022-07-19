4c2d7393a282a48f202f810ad6e43d5ad3b8f88c
Barbara Szewczyk <comamysza@gmail.com>
2022-07-19 21:42:47 +0200

    adds sameFrequency challenge task

---
+++ sameFrequency-challenge.js
@@ -0,0 +1,7 @@
+// Write a function called sameFrequency. Given two positive integers find out if the two numbers have the same frequency of digits.
+// Your solution MUST have complexity O(n)
+
+// console.log(sameFrequency(182, 281)); // true
+// console.log(sameFrequency(34, 14)); // false
+// console.log(sameFrequency(3589578, 5879385)); // true
+// console.log(sameFrequency(22, 222)); // false
